a294ad615a533ab5ec85c39ef68fa1d0f30ed97e
Liam Davidson <ldavidson@lcs.on.ca>
2018-05-07 15:07:36 -0400

    I'm going to break this Mac.

--- Final Exam Practice.playground/Pages/Practice Task 5.xcplaygroundpage/Contents.swift
+++ Final Exam Practice.playground/Pages/Practice Task 5.xcplaygroundpage/Contents.swift
@@ -30,9 +30,45 @@ let canvas = Canvas(width: 400, height: 600)
 let offWhite = Color(hue: 81, saturation: 5, brightness: 88, alpha: 100)
 let limeGreen = Color(hue: 106, saturation: 64, brightness: 73, alpha: 100)
 
+
 // Begin your solution here...
 
+canvas.fillColor = Color.black
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 600)
 
+canvas.fillColor = limeGreen
+
+var switchToGreen = 80
+
+
+// loops
+for y in stride(from: 400, to: -40, by: -40) {
+    for x in stride(from: 0, to: 440, by: 40) {
+        
+
+        canvas.drawEllipse(centreX: x, centreY: y, width: 35, height: 35)
+        
+    }
+}
+
+var whiteNegative = 0
+
+for Y in stride(from: 40, to: 400, by: 40) {
+    for X in stride(from: 40, to: 400, by: 40) {
+    
+    canvas.fillColor = offWhite
+    
+    canvas.drawEllipse(centreX: X, centreY: Y - whiteNegative, width: 35, height: 35)
+    
+    
+    
+    
+    
+
+    
+}
+    whiteNegative -= 40
+}
 /*:
  **Remember to commit and push your work,please**.
  */
